34ebfe74d3af58af4c056cb850bccee9d3d14489
ak4422920 <ak4422920@gmail.com>
2025-12-27 17:21:47 +0530

    Update root route link from Silicon Bots to AkMovieVerse

--- web/stream_routes.py
+++ web/stream_routes.py
@@ -11,7 +11,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.Response(text='<h1 align="center"><a href="https://t.me/Silicon_Bot_Update"><b>Silicon Bots</b></a></h1>', content_type='text/html')
+    return web.Response(text='<h1 align="center"><a href="https://t.me/akmovieverse"><b>AkMovieVerse</b></a></h1>', content_type='text/html')
 
 @routes.get("/watch/{message_id}")
 async def watch_handler(request):
