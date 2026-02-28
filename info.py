69255ed6e0cf965ee993cda9bb52edcc2011e8ff
ak4422920 <ak4422920@gmail.com>
2026-02-28 22:27:12 +0530

    Update FILE_AUTO_DEL_TIMER default value to 2400

--- info.py
+++ info.py
@@ -92,7 +92,7 @@ LANDSCAPE_POSTER = bool(environ.get('LANDSCAPE_POSTER', True)) # Shows landscape
 # Bot Settings
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
-FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '2200'))
+FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '2400'))
 PORT = os.environ.get('PORT', '5000')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
